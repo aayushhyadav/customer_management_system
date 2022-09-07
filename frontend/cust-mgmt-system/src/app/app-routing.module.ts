4f349ddaa3de35008e8788cc0a0b86a1430869bd
Aayush <aayushmum@gmail.com>
2022-09-07 08:05:38 +0530

    feat: boilerplate code for header, nav, view-customer and routing

--- frontend/cust-mgmt-system/src/app/app-routing.module.ts
+++ frontend/cust-mgmt-system/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { ViewCustomerComponent } from './view-customer/view-customer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'create-customer', component:CreateCustomerComponent},
+  {path:'view-customer', component:ViewCustomerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/cust-mgmt-system/src/app/app.module.ts
+++ frontend/cust-mgmt-system/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { ViewCustomerComponent } from './view-customer/view-customer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateCustomerComponent
+    CreateCustomerComponent,
+    ViewCustomerComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
